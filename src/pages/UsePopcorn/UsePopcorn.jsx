3744b07a21c6bf3af72d39545b97b8f7f4fdda20
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2025-12-05 18:07:15 +0200

    Clean up data fetching

--- src/pages/UsePopcorn/UsePopcorn.jsx
+++ src/pages/UsePopcorn/UsePopcorn.jsx
@@ -13,59 +13,12 @@ import Loader from "../../components/Loader/Loader.jsx";
 import ErrorText from "./components/ErrorText.jsx";
 import MovieDetails from "./components/MovieDetails";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    Runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-        "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    Runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const KEY = import.meta.env.VITE_OMBD_API_KEY
 
 export default function UsePopcorn() {
-  const [movies, setMovies] = useState(tempMovieData)
+  const [movies, setMovies] = useState([])
   const [watchedMovies, setWatchedMovies] = useState([])
-  const [searchQuery, setSearchQuery] = useState('interstellar')
+  const [searchQuery, setSearchQuery] = useState('')
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState('')
   const [selectedId, setSelectedId] = useState(null)
@@ -83,20 +36,24 @@ export default function UsePopcorn() {
   }
 
   useEffect(() => {
+    const controller = new AbortController()
+
     async function fetchMovies() {
       try {
         setIsLoading(true)
         setError('')
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${searchQuery}`)
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${searchQuery}`, {signal: controller.signal})
         if (!res.ok) throw new Error("Something went wrong with fetching movies")
 
         const data = await res.json()
         if (data.Response === 'False') throw new Error("Movie not found")
 
         setMovies(data.Search)
-        setIsLoading(false)
+        setError('')
       } catch (err) {
-        setError(err.message)
+        if (err.name !== 'AbortError') {
+          setError(err.message)
+        }
       } finally {
         setIsLoading(false)
       }
@@ -109,6 +66,12 @@ export default function UsePopcorn() {
     }
 
     fetchMovies()
+
+    // Each time we type a letter, a request is sent to API
+    // This will make sure that only the last request returns a response
+    return function () {
+      controller.abort()
+    }
   }, [searchQuery])
 
   useEffect(() => {
