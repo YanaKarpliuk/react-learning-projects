22e7f414e900872f128d081baae3315c6195d597
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2025-12-07 00:35:16 +0200

    Add focus to search

--- src/pages/UsePopcorn/components/Search.jsx
+++ src/pages/UsePopcorn/components/Search.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({setSearchQuery, searchQuery}) {
+  const searchRef = useRef(null)
+
+  useEffect(() => {
+    searchRef.current.focus()
+
+    function callback (e) {
+      if (e.code === 'Enter') searchRef.current.focus()
+    }
+
+    document.addEventListener('keydown', callback)
+
+    return function() {
+      document.removeEventListener('keydown', callback)
+    }
+  }, [])
+
   return (
       <div className='search-wrapper'>
         <label htmlFor='search' className='sr-only'>Search</label>
@@ -7,6 +25,7 @@ export default function Search({setSearchQuery, searchQuery}) {
             placeholder='Search movies...'
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            ref={searchRef}
         />
       </div>
   )
